69ef4b4e5ecf2b3526564dafa4eaddf31a037f9a
nvpraz0r <32584149+nvpraz0r@users.noreply.github.com>
2022-07-01 16:22:26 -0500

    Update to multiple classes
    Adding functionality to DBUtils while trying to fix an erroneous bug in LoggedInController class

--- src/main/java/sample/DBUtils.java
+++ src/main/java/sample/DBUtils.java
@@ -1,4 +1,118 @@
 package sample;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.sql.*;
+
+/**
+ * DBUtils class changes the scene for the application and does all the communication with the database
+ *
+ */
 public class DBUtils {
+
+    public static void changeScene(ActionEvent event, String fxmlFile, String title, String username, String super_pw){
+        Parent root = null;
+
+        if(username != null && super_pw != null){
+            try{
+                FXMLLoader loader = new FXMLLoader(DBUtils.class.getResource(fxmlFile));
+                root = loader.load();
+                LoggedInController loggedInController = loader.getController();
+                loggedInController.setUserInformation(username, super_pw);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        } else {
+            try{
+                root = FXMLLoader.load(DBUtils.class.getResource(fxmlFile));
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle(title);
+        stage.setScene(new Scene(root, 600,400));
+        stage.show();
+    }
+
+    public static void signUpUser(ActionEvent event, String username, String password, String super_pw){
+        Connection connection = null;
+        PreparedStatement psInsert = null;
+        PreparedStatement psCheckUserExists = null;
+        ResultSet resultSet = null;
+
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javafx", "root", "kingslayer");
+            psCheckUserExists = connection.prepareStatement("SELECT * FROM users WHERE username = ?");
+            psCheckUserExists.setString(1, username);
+            resultSet = psCheckUserExists.executeQuery();
+
+            if (resultSet.isBeforeFirst()){
+                System.out.println("User already exists");
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("Username already exists");
+                alert.show();
+            } else {
+                psInsert = connection.prepareStatement("INSERT INTO users (username, password, super_pw VALUES (?, ?, ?)");
+                psInsert.setString(1, username);
+                psInsert.setString(2, password);
+                psInsert.setString(3, super_pw);
+                psInsert.executeUpdate();
+
+                changeScene(event, "logged-in.fxml", "Welcome!", username, super_pw);
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        } finally {
+            if(resultSet != null){
+                try{
+                    resultSet.close();
+                } catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+            if(psCheckUserExists != null){
+                try{
+                    psCheckUserExists.close();
+                } catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+            if(psInsert != null){
+                try{
+                    psInsert.close();
+                } catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+            if(connection != null){
+                try{
+                    connection.close();
+                } catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void logInUser(ActionEvent event, String username, String password){
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javafx", "root", "kingslayer");
+//            preparedStatement = connection.prepareStatement("")
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/sample/LoggedInController.java
+++ src/main/java/sample/LoggedInController.java
@@ -1,9 +1,10 @@
 package sample;
 
+import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.event.ActionEvent;
+
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -25,7 +26,7 @@ public class LoggedInController implements Initializable{
         button_logout.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-
+                DBUtils.changeScene(actionEvent, "sample.fxml", "Log In", null, null);
             }
         });
     }
